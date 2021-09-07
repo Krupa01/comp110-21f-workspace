ea1f12fca6c54c5c688f0eb9c0d614d231f602e5
Krupa Makwana <krupa01@email.unc.edu>
2021-09-06 23:25:23 -0400

    progress on ex02

--- exercises/ex02/count_letters.py
+++ exercises/ex02/count_letters.py
@@ -5,12 +5,11 @@ __author__ = "730385108"
 
 letter = input("What letter do you want to seach for?: ")
 word = input("Enter a word: ")
-count = 0
+counter = 0
 i = 0
 
-while i < len(word):
+while len(word) > i:
     if(word[i] == letter):
-        count = count + 1
+        counter = counter + 1
     i = i + 1
-
-print("Count:", count)
+print("Count:", counter)
