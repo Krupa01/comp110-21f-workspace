b0a3cbe9178faf282ace2818a23d40c50b13fd63
Krupa Makwana <krupa01@email.unc.edu>
2021-09-06 23:08:56 -0400

    Progress on ex02

--- exercises/ex02/repeat_beat.py
+++ exercises/ex02/repeat_beat.py
@@ -3,11 +3,11 @@
 __author__ = "730385108"
 
 
-beat = str = input("What beat do you want to repeat? ")
-repeat = int = int(input("How many times do you want to repeat it? "))
-counter = int = 0
+beat: str = input("What beat do you want to repeat? ")
+repeat: int = int(input("How many times do you want to repeat it? "))
+counter: int = 0
 if counter < repeat:
-    print(((beat + " ") * (repeat - 1)), beat)
+    print(((beat + " ") * (repeat - 1)) + beat)
 else: 
     if repeat <= 0:
         print("No beat...")
