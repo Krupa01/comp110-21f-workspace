81a5524b1239f602f0f40ad0cfbc5b58ab78cb74
Krupa Makwana <krupa01@email.unc.edu>
2021-09-12 20:03:08 -0400

    Progress on Exercise 3

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -1,3 +1,17 @@
 """Finding duplicate letters in a word."""
 
-__author__ = "123456789"
+__author__ = "730385108"
+
+word: str = input("Enter a word: ")
+dup: bool = False
+
+i: int = 0
+
+while len(word) > i:
+    j: int = i + 1
+    while j < len(word):
+        if(word[i] == word[j]):
+            dup = True 
+        j += 1
+    i += 1
+print("Found duplicate:", dup)
--- exercises/ex03/happy_trees.py
+++ exercises/ex03/happy_trees.py
@@ -1,6 +1,11 @@
 """Drawing forests in a loop."""
 
-__author__ = "123456789"
+__author__ = "1730385108"
 
 # The string constant for the pine tree emoji
 TREE: str = '\U0001F332'
+number: int = int(input("Depth: "))
+i: int = 1
+while i <= number: 
+    print(TREE * i)
+    i += 1
