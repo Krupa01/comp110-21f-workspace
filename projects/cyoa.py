b1a467c0bd2646c4f9097b9fca3159b7468d1131
Krupa Makwana <krupa01@email.unc.edu>
2021-09-20 20:19:45 -0400

    progress on cyoa

--- projects/cyoa.py
+++ projects/cyoa.py
@@ -11,7 +11,7 @@ UP: str = u"\u2B06\uFE0F"
 DOWN: str = u"\u2B07\uFE0F"
 YAY: str = u"\U000FE517"
 
-points: int = 1
+points: int = 0
 player: str = ("name")
 
 
@@ -26,89 +26,42 @@ def main() -> None:
     """DOCstring."""
     greet()
     global points
-    print("You have 5 attempts to guess the correct number. The less points the better!")
+    print("You will earn a point for each correct guess!")
+    prompt: str = ""
     i = int(0)
     while i < 1000:
-        prompt: str = input("Would you like to guess the magic number between a) 1-10, b) 1-50, or c) 1-100 ? Please type a , b, or c. \n "
-                            "If you would like to leave the game type 'quit'.")
-        if prompt == "a" or "A":
-            game_a(10)
-        elif prompt == "b" or "B":
-            game_b(50)
-        elif prompt == "c" or "C":
-            game_c(100)
-        elif prompt == "quit" or "Quit": 
+        prompt = str(input("Would you like to guess the magic number between a) 1-10, b) 1-50, or c) 1-100 ? Please type a , b, or c. If you would like to leave the game type 'quit'. "))
+        print(prompt)
+        if prompt == "a":
+            game(10, 5)
+        elif prompt == "b":
+            game(50, 5)
+        elif prompt == "c":
+            game(100, 5)
+        elif prompt == "quit": 
             quit_game()
-        i += 1
-    if points > 5:
-        print(f"Sorry {player} you lost! You had {points} attempts {SAD}")
-    else:
-        print(f"You won with {points} attempts!!! {SMILE}")
 
 
-def game_a(n: int) -> int:
+def game(n: int, a: int) -> int:
     """This is the start_game..."""
-    global points 
-    guess = int(input("What's your guess?: "))
-    magic_number = random.randint(1, 10)
-    attempts = 5
-    while guess != magic_number and attempts > points:
-        if magic_number < guess:
-            print(f"Guess lower {DOWN}")
-            points += 1
-        elif magic_number > guess:
-            print(f"Guess higher {UP}") 
-            points += 1
-        guess = int(input("What's your guess?: "))
-    if points >= attempts:
-        print(f"Sorry you lost! You ran out of attempts {SAD}")
-    elif magic_number == guess:
-        print("You guessed it! The number was", magic_number)
-        print(f"You won with {points} attempts!!! {SMILE}")
-    return(points)
-
-
-def game_b(n: int) -> int:
-    """This is the start_game..."""
-    global points 
-    guess = int(input("What's your guess?: "))
-    magic_number = random.randint(1, 50)
-    attempts = 5
-    while guess != magic_number and attempts > points:
-        if magic_number < guess:
-            print(f"Guess lower {DOWN}")
-            points += 1
-        elif magic_number > guess:
-            print(f"Guess higher {UP}") 
-            points += 1
-        guess = int(input("What's your guess?: "))
-    if points >= attempts:
-        print(f"Sorry you lost! You ran out of attempts {SAD}")
-    elif magic_number == guess:
-        print("You guessed it! The number was", magic_number)
-        print(f"You won with {points} attempts!!! {SMILE}")
-    return(points)
-
-
-def game_c(n: int) -> int:
-    """This is the start_game..."""
+    print(f"You choose to guess a number that is between 1 - {n}.")
     global points
-    guess = int(input("What's your guess?: "))
-    magic_number = random.randint(1, 100)
-    attempts = 5
-    while guess != magic_number and attempts > points:
-        if magic_number < guess:
-            print(f"Guess lower {DOWN}")
-            points += 1
-        elif magic_number > guess:
-            print(f"Guess higher {UP}") 
-            points += 1
+    magic_number = random.randint(1, n)
+    t: int = 0
+    while t < a:
         guess = int(input("What's your guess?: "))
-    if points >= attempts:
-        print(f"Sorry you lost! You ran out of attempts {SAD}")
-    elif magic_number == guess:
-        print("You guessed it! The number was", magic_number)
-        print(f"You won with {points} attempts!!! {SMILE}")
+        if guess == magic_number:
+            print("You got it...")
+            points += 1
+            return points
+        else:
+            if guess > magic_number:
+                print("Too high")
+            else:
+                print("too low")
+        t += 1
+    print("Out of attempts, try playing again.")
+    print(f"So far you have {points} points.")
     return(points)
 
 
