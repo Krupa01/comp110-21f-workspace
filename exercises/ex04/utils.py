d19ceff30158f2e7cd27833211371565729ba8a9
Krupa Makwana <krupa01@email.unc.edu>
2021-09-29 01:54:09 -0400

    Progress for EX 04

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -19,18 +19,19 @@ def all(x: list[int], y: int, ) -> bool:
 
 def is_equal(x: list[int], y: list[int]) -> bool:
     """Function two returns True if both lists are the same."""
-    i: int = 0 
-    j: bool = False 
-    if len(x) = 0 
-    while i < len(x) and i < len(y):
+    it: int = 0 
+    ji: bool = False 
+    if len(x) == 0:
+        print("Insert something here once u fix whatever you are tryign to do something") 
+    while it < len(x) and it < len(y):
         if len(x) == len(y):
-            if x[i] == y[i]:
-                j = True
-                i += 1
+            if x[it] == y[it]:
+                ji = True
+                it += 1
             else:
-                j = False
+                ji = False
                 return False
-    return j 
+    return ji 
 
 
 def max(input: list[int]) -> int:
