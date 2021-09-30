5d5c642d6d268fccfba9abc9a522594943d35fb9
Krupa Makwana <krupa01@email.unc.edu>
2021-09-29 22:57:40 -0400

    Prog on EX04

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -40,9 +40,13 @@ def max(input: list[int]) -> int:
     """The last functions find the max in the list.""" 
     if len(input) == 0:
         raise ValueError("max() arg is an empty List") 
-    else:
-        maxim = input[0]
-        for number in input:
-            if number > maxim:
-                maxim = number
-    return maxim 
+    i: int = 1
+    maximi: int = input[i]
+    while i < len(input):
+        maximi = input[0]
+        if input[0] > input[i]:
+            i += 1
+        else:
+            maximi = input[i]
+            return maximi
+    return maximi 
