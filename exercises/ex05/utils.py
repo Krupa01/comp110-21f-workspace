164fef6b7b03d4e8b2b63818475b0b917711e0bc
Krupa Makwana <krupa01@email.unc.edu>
2021-10-03 19:57:32 -0400

    progress on ex05

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,42 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730385108"
 
-# Define your functions below
+
+def only_evens(x: list[int]) -> list[int]:
+    """This function will print the even numbers in the list."""
+    i: int = 0
+    evens = list()
+    while (i < len(x)):
+        if x[i] % 2 == 0: 
+            evens.append(x[i])
+            i += 1
+        else:
+            i += 1
+    return evens
+
+
+def sub(z: list[int], x: int, y: int) -> list[int]: 
+    """This function creates a subset"""
+    i: int = 0
+    lists = list()
+    end: int = y
+    start: int = x
+    if len(z) == 0 or end <= 0 or len(z) < start:
+        return lists
+    elif start >= 0: 
+        while end > start: 
+            lists.append(z[start])
+            start += 1 
+        return lists
+    elif start < 0: 
+        while end > i:
+            lists.append(z[i])
+            i += 1
+        return lists
+    elif len(z) < end: 
+        while len(z) > start:
+            lists.append(z[start])
+            start += 1 
+        return lists 
+    return z
